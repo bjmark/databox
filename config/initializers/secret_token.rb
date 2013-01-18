9b90a41e38cdcfca0a40e104bb824c38c4ad42d2
pengweimin <bjmark@sina.com>
2013-01-18 16:03:28 +0800

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Databox::Application.config.secret_token = '83735ec02f0aa88ec8e0cb17ce5d7a8ebbf9d10ff6597bf43969dd02fe577a790218432944ced2e8b82055f8fd5a7a49f576936187df0fd5e5e69929264798d6'
