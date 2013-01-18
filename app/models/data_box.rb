7a5a64b00648639b1141a64a70604d7ba07b0c3f
pengweimin <bjmark@sina.com>
2013-01-18 17:05:51 +0800

    new file:   .rspec 	modified:   Gemfile 	new file:   Gemfile.lock 	new file:   app/models/data_box.rb 	modified:   config/database.yml 	new file:   spec/spec_helper.rb

---
+++ app/models/data_box.rb
@@ -0,0 +1,14 @@
+class DataBox
+  def initialize(name)
+    @name = name
+  end
+
+  def create_table
+    @table = {}
+    @table[:content] = a
+    @table[[:search] = b
+  end
+
+  def search(cond*)
+  end
+end
